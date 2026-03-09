05a3624a30dbf16db8556d5a76aac865c27cc873
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-03-09 08:30:01 +0100

    fix: path for templates

--- src/neuroframe/templates/__init__.py
+++ src/neuroframe/templates/__init__.py
@@ -1,12 +1,14 @@
 import cv2
 
+from pathlib import Path
+
 from ..mouse_data import Segmentation
 from ..registrator import convert_input
 
+TEMPLATES_DIR = Path(__file__).resolve().parent
 
-
-ALLEN_TEMPLATE: Segmentation = Segmentation("src/neuroframe/templates/allen_brain_25μm_ccf_2017.nii.gz")
-SUTURE_TEMPLATE = cv2.imread("src/neuroframe/templates/suture_template_t14.png", cv2.IMREAD_GRAYSCALE)
-BREGMA_TEMPLATE = convert_input(cv2.imread("src/neuroframe/templates/bregma_template_t14.png", cv2.IMREAD_GRAYSCALE))
-LAMBDA_TEMPLATE = convert_input(cv2.imread("src/neuroframe/templates/lambda_template_t14.png", cv2.IMREAD_GRAYSCALE))
+ALLEN_TEMPLATE: Segmentation = Segmentation(str(TEMPLATES_DIR / "allen_brain_25μm_ccf_2017.nii.gz"))
+SUTURE_TEMPLATE = cv2.imread(str(TEMPLATES_DIR / "suture_template_t14.png"), cv2.IMREAD_GRAYSCALE)
+BREGMA_TEMPLATE = convert_input(cv2.imread(str(TEMPLATES_DIR / "bregma_template_t14.png"), cv2.IMREAD_GRAYSCALE))
+LAMBDA_TEMPLATE = convert_input(cv2.imread(str(TEMPLATES_DIR / "lambda_template_t14.png"), cv2.IMREAD_GRAYSCALE))
 REF_TEMPLATES: tuple = (BREGMA_TEMPLATE, LAMBDA_TEMPLATE)
